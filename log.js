1d45210f20718df5b243fabf1bec00e6eb52cf10
Skhmt <Skhmt@users.noreply.github.com>
2018-04-24 16:44:16 -0700

    Update log.js

--- log.js
+++ log.js
@@ -6,7 +6,7 @@ if (typeof(module) === 'object' && module.exports) module.exports = log;
 function log (strings, ...keys) {
 
 	const apiRegex = /(\{(?:[A-Za-z]*\.?)*\s)([^\}]*)\}/g;
-	const isNode = !(typeof(window) === 'object');
+	const inBrowser = (typeof(window) === 'object');
 	
 	let outputBuilder = '';
 	let browserCss = [];
@@ -33,28 +33,7 @@ function log (strings, ...keys) {
 		(match, g1, g2) => {
 			let apiList = g1.slice(1, -1).split('.');
 			let output = '';
-			if (isNode) {
-				if (apiList.includes('red')) output += '\x1b[91m';
-				else if (apiList.includes('green')) output += '\x1b[92m';
-				else if (apiList.includes('blue')) output += '\x1b[94m';
-				else if (apiList.includes('cyan')) output += '\x1b[96m';
-				else if (apiList.includes('magenta')) output += '\x1b[95m';
-				else if (apiList.includes('yellow')) output += '\x1b[93m';
-				else if (apiList.includes('black')) output += '\x1b[90m';
-				else if (apiList.includes('white')) output += '\x1b[97m';
-
-				if (apiList.includes('bgRed')) output += '\x1b[101m';
-				else if (apiList.includes('bgGreen')) output += '\x1b[102m';
-				else if (apiList.includes('bgBlue')) output += '\x1b[104m';
-				else if (apiList.includes('bgCyan')) output += '\x1b[106m';
-				else if (apiList.includes('bgMagenta')) output += '\x1b[105m';
-				else if (apiList.includes('bgYellow')) output += '\x1b[103m';
-				else if (apiList.includes('bgBlack')) output += '\x1b[100m';
-				else if (apiList.includes('bgWhite')) output += '\x1b[107m';
-
-				if (apiList.includes('underline')) output += '\x1b[4m';
-			}
-			else {
+			if (inBrowser) {
 				output = '%c';
 				let css = '';
 				if (apiList.includes('red')) css += `color: ${_cssRed};`;
@@ -79,20 +58,42 @@ function log (strings, ...keys) {
 
 				browserCss.push(css);
 			}
+			else {
+				if (apiList.includes('red')) output += '\x1b[91m';
+				else if (apiList.includes('green')) output += '\x1b[92m';
+				else if (apiList.includes('blue')) output += '\x1b[94m';
+				else if (apiList.includes('cyan')) output += '\x1b[96m';
+				else if (apiList.includes('magenta')) output += '\x1b[95m';
+				else if (apiList.includes('yellow')) output += '\x1b[93m';
+				else if (apiList.includes('black')) output += '\x1b[90m';
+				else if (apiList.includes('white')) output += '\x1b[97m';
+
+				if (apiList.includes('bgRed')) output += '\x1b[101m';
+				else if (apiList.includes('bgGreen')) output += '\x1b[102m';
+				else if (apiList.includes('bgBlue')) output += '\x1b[104m';
+				else if (apiList.includes('bgCyan')) output += '\x1b[106m';
+				else if (apiList.includes('bgMagenta')) output += '\x1b[105m';
+				else if (apiList.includes('bgYellow')) output += '\x1b[103m';
+				else if (apiList.includes('bgBlack')) output += '\x1b[100m';
+				else if (apiList.includes('bgWhite')) output += '\x1b[107m';
+
+				if (apiList.includes('underline')) output += '\x1b[4m';
+			}
 
 			output += g2;
 
-			if (isNode) output += '\x1b[0m';
-			else {
+			if (inBrowser) {
 				browserCss.push('color:;background-color:;');
 				output += '%c';
 			}
+			else output += '\x1b[0m';
+
 			return output;
 		} // (match, g1, g2) => {
 	);
 
-	if (isNode) console.log(outputBuilder);
-	else console.log(outputBuilder, ...browserCss);
+	if (inBrowser) console.log(outputBuilder, ...browserCss);
+	else console.log(outputBuilder);
 
 	return cleanOutput;
 }
